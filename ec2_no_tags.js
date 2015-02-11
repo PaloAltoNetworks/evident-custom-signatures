a246d74fcc3e497207e88acf408d69c30b1d9bd6
billautomata <bill@evident.io>
2015-02-11 10:10:22 -0800

    added 2 signatures

---
+++ ec2_no_tags.js
@@ -0,0 +1,50 @@
+//
+// ec2_only_micro.js - @billautomata
+// PROVIDED AS IS WITH NO WARRANTY OR GUARANTEES
+//
+//  This signature checks each instance in a region and determines if there
+//  are tags set.
+//
+//  If the instance has no tags it returns a failure.  Otherwise it returns a
+//  pass with an indication of how many tags there are.
+//
+//  You can change the number of tags to suit your security policy.
+//
+
+dsl.configure(function(c) {
+  c.valid_regions     = ['us_east_1'];                  // Only run in us_east_1
+  c.display_as        = 'global';                       // Display as region global instead of region us_east_1
+  c.deep_inspection   = ['users'];                      // Required
+  c.unique_identifier = [{'user_name': 'user_id'}];     // Required
+});
+
+function perform(aws) {
+  try {
+
+    var region = aws.region;
+    var alerts  = [];
+    var reservations = aws.ec2.describe_instances().reservations;
+
+    var string_to_check_for = 'micro'
+
+    reservations.map(function(element){
+        element.instances.map(function(instance){
+
+          var tags_length = instance.tags.length
+
+            if(tags_length === 0){
+                var fail_message = 'instance with id ' + instance.instance_id + ' has no tags set.'
+                alerts.push(dsl.fail({message: fail_message}));
+            } else {
+                var pass_message = 'instance with id ' + instance.instance_id +' has tags set. It has ' + tags_length + ' tag(s).'
+                alerts.push(dsl.pass({message: pass_message}));
+            }
+
+        })
+    })
+    return alerts;
+
+  } catch (err) {
+    return dsl.error({ error: err.message });
+  }
+}
---
+++ ec2_only_micro.js
@@ -0,0 +1,48 @@
+//
+// ec2_only_micro.js - @billautomata
+// PROVIDED AS IS WITH NO WARRANTY OR GUARANTEES
+//
+//  This signature checks each instance in a region and determines if the
+//  name of the instance_type contains the string 'micro'
+//
+//  If the instance is not micro it returns a failed alert, otherwise it passes.
+//
+//  You can change the type of instance in the string_to_check_for variable
+//  to enforce your security policy.
+//
+
+dsl.configure(function(c) {
+  c.valid_regions     = ['us_east_1'];                  // Only run in us_east_1
+  c.display_as        = 'global';                       // Display as region global instead of region us_east_1
+  c.deep_inspection   = ['users'];                      // Required
+  c.unique_identifier = [{'user_name': 'user_id'}];     // Required
+});
+
+function perform(aws) {
+  try {
+
+    var region = aws.region;
+    var alerts  = [];
+    var reservations = aws.ec2.describe_instances().reservations;
+
+    var string_to_check_for = 'micro'
+
+    reservations.map(function(element){
+        element.instances.map(function(instance){
+
+            if(instance.instance_type.indexOf(string_to_check_for) === -1){
+                var fail_message = 'instance with id ' + instance.instance_id + ' is not a micro instance. It is of the type : ' + instance.instance_type
+                alerts.push(dsl.fail({message: fail_message}));
+            } else {
+                var pass_message = 'instance with id ' + instance.instance_id + ' is a micro instance.'
+                alerts.push(dsl.pass({message: pass_message}));
+            }
+
+        })
+    })
+    return alerts;
+
+  } catch (err) {
+    return dsl.error({ error: err.message });
+  }
+}
