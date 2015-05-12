cdf13442600b298389de76c2c639cfa653b240a4
John Martinez <john@johnmartinez.com>
2015-05-12 01:24:35 -0700

    new custom signature

---
+++ ec2_instance_profile_name_check.rb
@@ -0,0 +1,34 @@
+configure do |c|
+  c.deep_inspection   = [:instance_id, :profile_arn, :tags]
+  c.unique_identifier = [:instance_id]
+end
+
+def perform(aws)
+    
+    aws.ec2.describe_instances.reservations.map(&:instances).flatten.each do |instance|
+        
+        instance_id  = instance[:instance_id]
+        tags = instance[:tags]
+        
+        if instance[:iam_instance_profile]
+            
+            profile_arn = instance[:iam_instance_profile].arn
+            set_data(instance_id: instance_id, profile_arn: profile_arn, tags: tags)
+            
+            if (profile_arn =~ /arn:aws:iam::\d{12}:instance-profile\/Standard-Role/)
+                pass(message: "The correct instance profile is assigned to instance #{instance_id}", resource_id: instance_id)
+            else
+                fail(message: "The instance profile assigned to instance #{instance_id} does not conform to instance role standards", resource_id: instance_id)
+            end
+            
+        else
+            
+            set_data(instance_id: instance_id, profile_arn: profile_arn, tags: tags)
+            warn(message: "No instance profile assigned to instance #{instance_id}", resource_id: instance_id)
+            
+        end
+        
+    end
+
+end
+
