c5370c2879f7a4e8738791cba2c736c1938271c6
John Martinez <john@johnmartinez.com>
2016-11-01 09:10:08 -0700

    inspecting date of maintenance to generate FAIL within 7 days

--- ec2_instance_maintenance_event_check.rb
+++ ec2_instance_maintenance_event_check.rb
@@ -1,6 +1,8 @@
 ##
 ## ec2_instance_maintenance_event_check.rb - John Martinez (john@evident.io)
 ##
+## Name: Instance Maintenance Event Check
+##
 ## Severity Level: Medium
 ##
 ## Description
@@ -13,12 +15,14 @@
 ## 
 
 configure do |c|
-    c.deep_inspection   = [:instance_state, :events, :tags]
+    c.deep_inspection   = [:days_left, :instance_state, :events, :tags]
     c.unique_identifier = [:instance_id]
 end
 
 def perform(aws)
     
+    now = Time.now
+
     aws.ec2.describe_instances.reservations.map(&:instances).flatten.each do |instance|
         
         instance_id  = instance[:instance_id]
@@ -31,13 +35,27 @@ def perform(aws)
 
             instance_state = status.instance_state[:name]
             events = status.events
+            not_before = nil
             
-            set_data(instance_state: instance_state, events: events, tags: tags)
+            events.each do |event|
+                not_before = event[:not_before].to_datetime
+            end
             
+            delta = not_before.to_i - now.to_i
+            days_left = delta / 86400
+
             if instance_state == "running"
                 if events.count > 0
-                    warn(message: "Instance #{instance_id} has upcoming maintenance events", resource_id: instance_id)
+                    set_data(days_left: days_left, instance_state: instance_state, events: events, tags: tags)
+
+                    if days_left < 7
+                        fail(message: "Instance #{instance_id} has maintenance events in less than 7 days", resource_id: instance_id)
+                    else
+                        warn(message: "Instance #{instance_id} has upcoming maintenance events", resource_id: instance_id)
+                    end
                 else
+                    days_left = nil
+                    set_data(days_left: days_left, instance_state: instance_state, events: events, tags: tags)
                     pass(message: "Instance #{instance_id} has no upcoming maintenance events", resource_id: instance_id)
                 end
             end
