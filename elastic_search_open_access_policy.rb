e101b1d9779659099a919cbfeb02ab933b158991
John Martinez <john@johnmartinez.com>
2017-01-17 23:16:10 -0800

    added condition for no access policies on domain

--- elastic_search_open_access_policy.rb
+++ elastic_search_open_access_policy.rb
@@ -18,7 +18,7 @@
 configure do |c|
     c.deep_inspection = [:domain_id, :arn, :endpoint, :access_policies]
     c.valid_regions = [:us_east_1, :us_east_2, :us_west_1, :us_west_2, :ap_south_1, :ap_northeast_2,
-                        :ap_southeast_1, :ap_southeast_2, :ap_northeast_1, :eu_central_1,
+                        :ap_southeast_1, :ap_southeast_2, :ap_northeast_1, :eu_central_1, 
                         :eu_west_1, :sa_east_1]
     c.unique_identifier  = [:domain_name]
 end
@@ -36,37 +36,44 @@ def perform(aws)
         
         domain_status_list.each do |domain_status|
             
+            policy_doc = nil
+            access_policies = nil
+            
             domain_id  = domain_status[:domain_id]
             arn = domain_status[:arn]
             endpoint = domain_status[:endpoint]
             policy_doc = domain_status[:access_policies]
-            access_policies = JSON.parse(URI.decode(policy_doc))
             
             fail_count = 0
             
-            policy = access_policies.Statement
+            if policy_doc != ""
+                
+                access_policies = JSON.parse(URI.decode(policy_doc))
+                policy = access_policies.Statement
             
-            effect = nil
-            principal = nil
-            action = nil
-            condition = nil
-            source_ip = nil
+                effect = nil
+                principal = nil
+                action = nil
+                condition = nil
+                source_ip = nil
 
-            policy.each do |policy_statement|
+                policy.each do |policy_statement|
                 
-                effect = policy_statement["Effect"]
-                principal = policy_statement["Principal"]["AWS"]
-                action = policy_statement["Action"]
-                condition = policy_statement["Condition"]
+                    effect = policy_statement["Effect"]
+                    principal = policy_statement["Principal"]["AWS"]
+                    action = policy_statement["Action"]
+                    condition = policy_statement["Condition"]
                 
-                if condition != nil
-                    source_ip = policy_statement["Condition"]["IpAddress"]["aws:SourceIp"]
+                    if condition != nil
+                        source_ip = policy_statement["Condition"]["IpAddress"]["aws:SourceIp"]
+                    end
+                
+                    if effect == "Allow" && principal == "*" && action == "es:*" && source_ip == nil
+                        fail_count += 1
+                    end
+                
                 end
-                
-                if effect == "Allow" && principal == "*" && action == "es:*" && source_ip == nil
-                    fail_count += 1
-                end
-                
+            
             end
 
             set_data(domain_id: domain_id, arn: arn, endpoint: endpoint, access_policies: access_policies)
@@ -74,7 +81,11 @@ def perform(aws)
             if fail_count > 0
                 fail(message: "Elasticsearch domain #{domain_name} has an open access policy", resource_id: domain_name)
             else
-                pass(message: "Elasticsearch domain #{domain_name} has a restricted access policy", resource_id: domain_name)
+                if access_policies == nil
+                    pass(message: "Elasticsearch domain #{domain_name} has no access policies", resource_id: domain_name)
+                else
+                    pass(message: "Elasticsearch domain #{domain_name} has a restricted access policy", resource_id: domain_name)
+                end
             end
             
         end
